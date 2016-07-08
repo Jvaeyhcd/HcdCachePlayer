b88fa8a24d70eda03860245454f290d77de4a2f4
Jvaeyhcd <chedahuang@icloud.com>
2016-07-08 16:13:37 +0800

    screen

---
+++ hcdCachePlayer/HcdLightView.h
@@ -0,0 +1,13 @@
+//
+//  HcdLightView.h
+//  hcdCachePlayerDemo
+//
+//  Created by polesapp-hcd on 16/7/8.
+//  Copyright © 2016年 Polesapp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HcdLightView : UIView
+
+@end
---
+++ hcdCachePlayer/HcdLightView.m
@@ -0,0 +1,13 @@
+//
+//  HcdLightView.m
+//  hcdCachePlayerDemo
+//
+//  Created by polesapp-hcd on 16/7/8.
+//  Copyright © 2016年 Polesapp. All rights reserved.
+//
+
+#import "HcdLightView.h"
+
+@implementation HcdLightView
+
+@end
